419be7fbbce2674c08386f8aa0b35094c5f345ae
Alex-Pennington <53480806+Alex-Pennington@users.noreply.github.com>
2024-11-02 04:26:59 -0400

    Added swSerial, LCD
    HiLetgo 1602 LCD Keypad Shield

--- ArduinoHydraulicScale.ino
+++ ArduinoHydraulicScale.ino
@@ -1,13 +1,29 @@
+
+
 // Author: alex.pennington@organicengineer.com
 
 #define calWeight 530 // the weight of the calibration object
 
 #include <EEPROM.h>
 
+#include <SoftwareSerial.h>
+SoftwareSerial swSerial(2, 3); // RX, TX
+
+#include <LiquidCrystal.h>
+//LCD pin to Arduino
+const int pin_RS = 8;
+const int pin_EN = 9;
+const int pin_d4 = 4;
+const int pin_d5 = 5;
+const int pin_d6 = 6;
+const int pin_d7 = 7;
+const int pin_BL = 10;
+LiquidCrystal lcd( pin_RS,  pin_EN,  pin_d4,  pin_d5,  pin_d6,  pin_d7);
+
 bool debug_flag = false;
 
 // These constants won't change. They're used to give names to the pins used:
-const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+const int analogInPin = A2;  // Analog input pin that the potentiometer is attached to
 
 float vADC;  // the voltage
 float fValue;  //the filtered value from the last reading
@@ -15,7 +31,7 @@ float weighting = 0.7; //the weight (between 0 and 1) given to the previous filt
 float nWeighting;  //the weighting given to the new input value. weighting + nWeighting must equal 1 so we just calculate nWeighting as 1 - weighting
 float vReference = 5.0; //using the supply voltage as the reference
 unsigned long previousMillis = 0;
-int interval = 10; //choose the time between readings in milliseconds
+int interval = 100; //choose the time between readings in milliseconds
 
 
 int sensorValue = 0;
@@ -31,6 +47,11 @@ unsigned int rTsum = 0;
 String inputString = "";         // a String to hold incoming data
 bool stringComplete = false;  // whether the string is complete
 
+
+unsigned long button_LastChange = 0;
+unsigned long button_DebounceTime = 1000;
+int button_PressedIndex = 0;
+int button_LastPressedIndex = 0;
 void setup() {
   // initialize serial communications at 9600 bps:
   delay(2000);
@@ -40,19 +61,59 @@ void setup() {
   nWeighting = 1 - weighting; //calculate weight to be applied to the new reading
   fValue = analogRead(analogInPin);  //initialise to current value of input
   //EEPROM.get(addr, calFactor);
+
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("Loader Scale");
+  lcd.setCursor(0, 1);
+  //lcd.print("Press Key:");
+
+  swSerial.begin(4800);
+  swSerial.println("JD5055e OK");
 }
 
 void loop() {
   unsigned long currentMillis = millis();  //get the time
+  
   if (currentMillis - previousMillis >= interval) {  // if its time for a new reading
     previousMillis = currentMillis;                  //update time of most recent reading
     sensorValue = analogRead(analogInPin);                    // read the input at the analog pin
     fValue = sensorValue * nWeighting + fValue * weighting; //calculate new fValue based on input and previous fValue
     pValue = map(fValue, tareValue, 1023, 0, 3000);
     outputValue = calFactor * pValue;
+    lcd.setCursor(0, 1);
+    lcd.print ("                 ");
+    lcd.setCursor(0, 1);
+    lcd.print (outputValue);
   }
+
+  if ((currentMillis - button_LastChange) >  button_DebounceTime) {
+    int x;
+    x = analogRead (0);
+    lcd.setCursor(1, 1);
+    if (x < 60) {
+      lcd.print ("Right ");
+    }
+    else if (x < 200) {
+      lcd.print ("Up    ");
+    }
+    else if (x < 400) {
+      lcd.print ("Down  ");
+    }
+    else if (x < 600) {
+      lcd.print ("Left  ");
+    }
+    else if (x < 800) {
+      button_PressedIndex = 5;
+      lcd.print (outputValue);
+      swSerial.println(outputValue);
+    }
+    button_LastChange = currentMillis;
+    button_LastPressedIndex = button_PressedIndex;
+  }
+
   if (stringComplete) {
-    Serial.println(inputString);
+    //Serial.println(inputString);
     if (inputString == "tare") {
       tareValue = fValue;
     }
@@ -120,9 +181,6 @@ void loop() {
     stringComplete = false;
   }
 
-  // wait 2 milliseconds before the next loop for the analog-to-digital
-  // converter to settle after the last reading:
-  delay(2);
 }
 
 /*
